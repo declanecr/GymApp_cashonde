0116f6752da8aecbabf296fea488c42c7337169b
declanecr <declan.cr04@gmail.com>
2024-10-11 16:49:57 -0400

    added workout model and controller files

---
+++ app/controllers/workout.controller.js
@@ -0,0 +1,137 @@
+/**
+ * Workout Controller
+ *
+ * This file contains controller functions for handling workout-related operations.
+ * It manages the communication between the client requests and the Workout model,
+ * processing data and sending appropriate responses.
+ */
+
+import Workout from "../models/workout.model.js";
+
+/**
+ * Create and Save a new Workout
+ * 
+ * @param {Object} req - The request object
+ * @param {Object} res - The response object
+ */
+export const create = (req, res) => {
+  // Validate request
+  if (!req.body) {
+    res.status(400).send({
+      message: "Content can not be empty!"
+    });
+    return;
+  }
+
+  // Create a Workout
+  const workout = new Workout({
+    name: req.body.name,
+    date: req.body.date,
+    duration: req.body.duration,
+    user_id: req.body.user_id
+  });
+
+  // Save Workout in the database
+  Workout.create(workout, (err, data) => {
+    if (err)
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while creating the Workout."
+      });
+    else res.send(data);
+  });
+};
+
+/**
+ * Retrieve all Workouts from the database.
+ * 
+ * @param {Object} req - The request object
+ * @param {Object} res - The response object
+ */
+export const findAll = (req, res) => {
+  Workout.getAll((err, data) => {
+    if (err)
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while retrieving workouts."
+      });
+    else res.send(data);
+  });
+};
+
+/**
+ * Find a single Workout with a workoutId
+ * 
+ * @param {Object} req - The request object
+ * @param {Object} res - The response object
+ */
+export const findOne = (req, res) => {
+  Workout.findById(req.params.workoutId, (err, data) => {
+    if (err) {
+      if (err.kind === "not_found") {
+        res.status(404).send({
+          message: `Not found Workout with id ${req.params.workoutId}.`
+        });
+      } else {
+        res.status(500).send({
+          message: "Error retrieving Workout with id " + req.params.workoutId
+        });
+      }
+    } else res.send(data);
+  });
+};
+
+/**
+ * Update a Workout identified by the workoutId in the request
+ * 
+ * @param {Object} req - The request object
+ * @param {Object} res - The response object
+ */
+export const update = (req, res) => {
+  // Validate Request
+  if (!req.body) {
+    res.status(400).send({
+      message: "Content can not be empty!"
+    });
+  }
+
+  Workout.updateById(
+    req.params.workoutId,
+    new Workout(req.body),
+    (err, data) => {
+      if (err) {
+        if (err.kind === "not_found") {
+          res.status(404).send({
+            message: `Not found Workout with id ${req.params.workoutId}.`
+          });
+        } else {
+          res.status(500).send({
+            message: "Error updating Workout with id " + req.params.workoutId
+          });
+        }
+      } else res.send(data);
+    }
+  );
+};
+
+/**
+ * Delete a Workout with the specified workoutId in the request
+ * 
+ * @param {Object} req - The request object
+ * @param {Object} res - The response object
+ */
+export const remove = (req, res) => {
+  Workout.remove(req.params.workoutId, (err, data) => {
+    if (err) {
+      if (err.kind === "not_found") {
+        res.status(404).send({
+          message: `Not found Workout with id ${req.params.workoutId}.`
+        });
+      } else {
+        res.status(500).send({
+          message: "Could not delete Workout with id " + req.params.workoutId
+        });
+      }
+    } else res.send({ message: `Workout was deleted successfully!` });
+  });
+};
---
+++ app/models/workout.model.js
@@ -0,0 +1,106 @@
+/**
+ * Workout Model
+ * 
+ * This file defines the Workout model and its associated methods for interacting with the database.
+ * It provides functionality for creating, reading, updating, and deleting workout records.
+ */
+
+import sql from "./db.js";
+
+// Constructor
+const Workout = function(workout) {
+  this.name = workout.name;
+  this.date = workout.date;
+  this.exercises = workout.exercises;
+};
+
+// Create a new Workout
+Workout.create = (newWorkout, result) => {
+  sql.query("INSERT INTO workouts SET ?", newWorkout, (err, res) => {
+    if (err) {
+      console.log("error: ", err);
+      result(err, null);
+      return;
+    }
+    
+    console.log("created workout: ", { id: res.insertId, ...newWorkout });
+    result(null, { id: res.insertId, ...newWorkout });
+  });
+};
+
+// Find a Workout by ID
+Workout.findById = (id, result) => {
+  sql.query(`SELECT * FROM workouts WHERE id = ${id}`, (err, res) => {
+    if (err) {
+      console.log("error: ", err);
+      result(err, null);
+      return;
+    }
+    
+    if (res.length) {
+      console.log("found workout: ", res[0]);
+      result(null, res[0]);
+      return;
+    }
+    
+    result({ kind: "not_found" }, null);
+  });
+};
+
+// Get all Workouts
+Workout.getAll = (result) => {
+  sql.query("SELECT * FROM workouts", (err, res) => {
+    if (err) {
+      console.log("error: ", err);
+      result(null, err);
+      return;
+    }
+    
+    console.log("workouts: ", res);
+    result(null, res);
+  });
+};
+
+// Update a Workout by ID
+Workout.updateById = (id, workout, result) => {
+  sql.query(
+    "UPDATE workouts SET name = ?, date = ?, exercises = ? WHERE id = ?",
+    [workout.name, workout.date, JSON.stringify(workout.exercises), id],
+    (err, res) => {
+      if (err) {
+        console.log("error: ", err);
+        result(null, err);
+        return;
+      }
+      
+      if (res.affectedRows == 0) {
+        result({ kind: "not_found" }, null);
+        return;
+      }
+      
+      console.log("updated workout: ", { id: id, ...workout });
+      result(null, { id: id, ...workout });
+    }
+  );
+};
+
+// Delete a Workout
+Workout.remove = (id, result) => {
+  sql.query("DELETE FROM workouts WHERE id = ?", id, (err, res) => {
+    if (err) {
+      console.log("error: ", err);
+      result(null, err);
+      return;
+    }
+    
+    if (res.affectedRows == 0) {
+      result({ kind: "not_found" }, null);
+      return;
+    }
+    
+    console.log("deleted workout with id: ", id);
+    result(null, res);
+  });
+};
+
+export default Workout;
