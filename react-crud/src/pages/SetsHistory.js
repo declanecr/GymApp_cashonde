b572d8d7a48e1e5261ea2b16604d6e63282b7a6f
declanecr <declan.cr04@gmail.com>
2024-10-15 19:34:54 -0400

    creation of 'SetsHistory.js' as a page to view sets data

---
+++ react-crud/src/pages/SetsHistory.js
@@ -0,0 +1,46 @@
+/*  SetsHistory.js
+    The page for displaying the set data of a 
+    specific exercise found from 
+    /api/exercises/:id/sets
+
+*/
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import SetsGrid from '../components/SetsGrid';
+import ExerciseDataService from "../services/ExerciseDataService";
+
+const SetsHistory = () => {
+  const [sets, setSets] = useState([]);
+  const [exercise, setExercise] = useState(null);
+  const { id } = useParams();
+
+  useEffect(() => {
+    fetchExercise();
+    fetchSets();
+  }, [id]);
+
+  const fetchExercise = () => {
+    ExerciseDataService.get(id)
+      .then(response => {
+        setExercise(response.data);
+      })
+      .catch(error => console.error('Error fetching exercise:', error));
+  };
+
+  const fetchSets = () => {
+    ExerciseDataService.getAllSets(id)
+      .then(response => {
+        setSets(response.data);
+      })
+      .catch(error => console.error('Error fetching sets:', error));
+  };
+
+  return (
+    <div>
+      <h2>{exercise ? exercise.name : 'Loading...'} Sets History</h2>
+      <SetsGrid sets={sets} />
+    </div>
+  );
+};
+
+export default SetsHistory;
