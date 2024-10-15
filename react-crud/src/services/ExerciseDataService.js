5cf38fba312f21660a9336d937909d8a2a4a40dd
declanecr <declan.cr04@gmail.com>
2024-10-15 18:40:35 -0400

    updated 'ExerciseDataService.js' to handle the new URL

--- react-crud/src/services/ExerciseDataService.js
+++ react-crud/src/services/ExerciseDataService.js
@@ -165,6 +165,49 @@ const ExerciseDataService = {
    */
   getCurrentWorkout() {
     return JSON.parse(localStorage.getItem("currentWorkout")) || [];
+  },
+  /**
+   * Creates a new set for an exercise
+   * Purpose: Add a new set to a specific exercise
+   * Inputs: exerciseId - The ID of the exercise, setData - Object containing set details
+   * Outputs: Promise resolving to the created set object
+   * API Call: POST /exercises/:id/sets
+   */
+  createSet: (exerciseId, setData) => {
+    return http.post(`/exercises/${exerciseId}/sets`, setData);
+  },
+
+  /**
+   * Fetches all sets for an exercise
+   * Purpose: Retrieve all sets associated with a specific exercise
+   * Inputs: exerciseId - The ID of the exercise
+   * Outputs: Promise resolving to an array of set objects
+   * API Call: GET /exercises/:id/sets
+   */
+  getSetsForExercise: (exerciseId) => {
+    return http.get(`/exercises/${exerciseId}/sets`);
+  },
+
+  /**
+   * Updates an existing set
+   * Purpose: Modify details of an existing set for a specific exercise
+   * Inputs: exerciseId - The ID of the exercise, setId - The ID of the set, setData - Object with updated set details
+   * Outputs: Promise resolving to the updated set object
+   * API Call: PUT /exercises/:exerciseId/sets/:setId
+   */
+  updateSet: (exerciseId, setId, setData) => {
+    return http.put(`/exercises/${exerciseId}/sets/${setId}`, setData);
+  },
+
+  /**
+   * Deletes a set
+   * Purpose: Remove a set from a specific exercise
+   * Inputs: exerciseId - The ID of the exercise, setId - The ID of the set to delete
+   * Outputs: Promise resolving to the server response
+   * API Call: DELETE /exercises/:exerciseId/sets/:setId
+   */
+  deleteSet: (exerciseId, setId) => {
+    return http.delete(`/exercises/${exerciseId}/sets/${setId}`);
   }
 };
 
