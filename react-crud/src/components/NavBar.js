1f62fcfd8bc5c4fb8d0081e3b1dd82ea038c88dc
declanecr <declan.cr04@gmail.com>
2024-11-04 15:44:19 -0500

    NavBar significantly improved

--- react-crud/src/components/NavBar.js
+++ react-crud/src/components/NavBar.js
@@ -6,30 +6,81 @@
  * It also wraps the main content in Routes for proper routing.
  */
 
-import { AppBar, Button, Container, Toolbar } from '@mui/material';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import MenuIcon from '@mui/icons-material/Menu';
+import { AppBar, Button, Container, IconButton, Menu, MenuItem, Toolbar, Typography } from '@mui/material';
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useAuth } from '../AuthContext'; // Assuming you have an AuthContext
 
 const NavBar = () => {
+  const { currentUser, logout } = useAuth();
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <AppBar position="static">
       <Container maxWidth="lg">
-        <Toolbar sx={{ justifyContent: 'space-between' }}> {/* Dispersing items over the space */}
-          <Button component={Link} to="/" color="inherit">
-            Home
-          </Button>
-          <Button component={Link} to="/exercises" color="inherit">
-            Exercises
-          </Button>
-          <Button component={Link} to="/add" color="inherit">
-            Add Exercise
-          </Button>
-          <Button component={Link} to="/current-workout" color="inherit">
-            Workouts
-          </Button>
-          <Button component={Link} to= "/users/:user_id" color="inherit">
-            UserName
-          </Button>
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Gym App
+          </Typography>
+          <Button color="inherit" component={Link} to="/">Home</Button>
+          <Button color="inherit" component={Link} to="/exercises">Exercises</Button>
+          <Button color="inherit" component={Link} to="/add">Add Exercise</Button>
+          <Button color="inherit" component={Link} to="/current-workout">Workouts</Button>
+          {currentUser ? (
+            <div>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleMenu}
+                color="inherit"
+              >
+                <AccountCircle />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+              >
+                <MenuItem onClick={handleClose}>Account</MenuItem>
+                <MenuItem onClick={handleClose}>Settings</MenuItem>
+                <MenuItem onClick={handleClose}>My Metrics</MenuItem>
+                <MenuItem onClick={() => { handleClose(); logout(); }}>Logout</MenuItem>
+              </Menu>
+            </div>
+          ) : (
+            <Button color="inherit" component={Link} to="/login">Login</Button>
+          )}
         </Toolbar>
       </Container>
     </AppBar>
