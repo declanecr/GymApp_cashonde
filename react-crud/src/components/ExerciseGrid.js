9342f776098f80bef8e2d5a32436dc5de90ff5d4
declanecr <declan.cr04@gmail.com>
2024-11-14 21:06:03 -0500

    fixed call of prod link in http-common

--- react-crud/src/components/ExerciseGrid.js
+++ react-crud/src/components/ExerciseGrid.js
@@ -59,7 +59,7 @@ const ExerciseGrid = ({ handleRowClick }) => {
       );
     }
 
-    console.log(filteredExercises);
+    //console.log(filteredExercises);
     setExercises(filteredExercises);
   };
 
