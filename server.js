1e3a2e2e2ba79d26dc1c9c8a0be1165f6e5d70bc
declanecr <declan.cr04@gmail.com>
2024-11-20 14:13:16 -0500

    fixed getSets issue

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ const corsOptions = {
     : ['http://localhost:3000', 'http://localhost:3001'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'Origin', 'Accept', 'x-user-id'],
   exposedHeaders: ['Access-Control-Allow-Origin'],
   optionsSuccessStatus: 200
 };
